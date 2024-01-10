9ba99b5eb6894a657669f1049d4466c01437e51c
shiri <irihsmoc@gmail.com>
2024-01-10 16:38:03 +0900

    update: php8対応

--- src/MagicQuotesGpcEmulator.php
+++ src/MagicQuotesGpcEmulator.php
@@ -8,10 +8,10 @@ class MagicQuotesGpcEmulator {
       return;
     }
 
-    $_GET = filter_var_array($_GET, FILTER_SANITIZE_MAGIC_QUOTES);
-    $_POST = filter_var_array($_POST, FILTER_SANITIZE_MAGIC_QUOTES);
-    $_COOKIE = filter_var_array($_COOKIE, FILTER_SANITIZE_MAGIC_QUOTES);
-    $_REQUEST = filter_var_array($_REQUEST, FILTER_SANITIZE_MAGIC_QUOTES);
+    $_GET = filter_var_array($_GET, FILTER_SANITIZE_ADD_SLASHES);
+    $_POST = filter_var_array($_POST, FILTER_SANITIZE_ADD_SLASHES);
+    $_COOKIE = filter_var_array($_COOKIE, FILTER_SANITIZE_ADD_SLASHES);
+    $_REQUEST = filter_var_array($_REQUEST, FILTER_SANITIZE_ADD_SLASHES);
 
     define('MagicQuotesGpcEmulatorApplied', true);
   }
